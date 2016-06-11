480b3d4df91d3363866d9b0efeba5e82f17ae6ce
sonam <sonamdawani@gmail.com>
2016-06-11 13:05:52 +0530

    version1 bug fix

--- src/gittest3/Gittest3.java
+++ src/gittest3/Gittest3.java
@@ -18,6 +18,7 @@ public class Gittest3 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("first version..");
+        System.out.println("v1.2");
     }
     
 }
